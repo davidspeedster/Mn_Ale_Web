40fe30efa0a79337aef6608feb59bd013b1cf754
Emahai <60546763+Emahai@users.noreply.github.com>
2022-02-23 01:19:59 +0300

    false dependences of Dashboard Controller has been removed

--- app/Http/Controllers/DashboardControllers/DashboardController.php
+++ app/Http/Controllers/DashboardControllers/DashboardController.php
@@ -2,14 +2,8 @@
 
 namespace App\Http\Controllers\DashboardControllers;
 
-use App\Employee;
 use App\Http\Controllers\Controller;
-use App\NewsFeed;
 use App\Edition;
-use App\ProjectCategory;
-use App\SocialMediaAccount;
-use App\WebLinks;
-use App\WebText;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
