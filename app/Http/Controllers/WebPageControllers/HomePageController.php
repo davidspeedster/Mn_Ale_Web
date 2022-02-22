7a2b53700bab8f062d1e3deb90460b2b77b924d8
Dawit Mahetemework <Dawitmahetemework@gmail.com>
2022-02-22 23:23:41 +0300

    HompageController has been optimized

--- app/Http/Controllers/WebPageControllers/HomePageController.php
+++ app/Http/Controllers/WebPageControllers/HomePageController.php
@@ -2,13 +2,11 @@
 
 namespace App\Http\Controllers\WebPageControllers;
 
-use App\Employee;
 use App\Edition;
 
-use Illuminate\Http\Request;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Config;
-use phpDocumentor\Reflection\Types\True_;
 
 class HomePageController extends Controller
 {
