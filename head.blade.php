3a91d0c042a5b1fbe8c0f5eb3838c26975ea8c0a
Etsub Zeleke <46195118+etsub@users.noreply.github.com>
2022-02-22 20:31:24 -0200

    Add files via upload

---
+++ head.blade.php
@@ -0,0 +1,22 @@
+
+        @yield('css')
+
+         <!-- CSS here -->
+         <link rel="stylesheet" href={{asset("assets/Dashboard/vendor/bootstrap/css/bootstrap.min.css")}}>
+         <link href={{asset("assets/Dashboard/vendor/fonts/circular-std/style.css")}} rel="stylesheet">
+         <link rel="stylesheet" href={{asset("assets/Dashboard/libs/css/style.css")}}>
+         <link rel="stylesheet" href={{asset("assets/Dashboard/vendor/fonts/fontawesome/css/fontawesome-all.css")}}>
+         <link rel="stylesheet" href={{asset("assets/Dashboard/vendor/charts/chartist-bundle/chartist.css")}}>
+         <link rel="stylesheet" href={{asset("assets/Dashboard/vendor/charts/morris-bundle/morris.css")}}>
+         <link rel="stylesheet" href={{asset("assets/Dashboard/vendor/fonts/material-design-iconic-font/css/materialdesignicons.min.css")}}>
+         <link rel="stylesheet" href={{asset("assets/Dashboard/vendor/charts/c3charts/c3.css")}}>
+         <link rel="stylesheet" href={{asset("assets/Dashboard/vendor/fonts/flag-icon-css/flag-icon.min.css")}}>
+
+
+    <link rel="stylesheet" type="text/css" href={{asset("assets/Dashboard/vendor/datatables/css/dataTables.bootstrap4.css")}}>
+    <link rel="stylesheet" type="text/css" href={{asset("assets/Dashboard/vendor/datatables/css/buttons.bootstrap4.css")}}>
+    <link rel="stylesheet" type="text/css" href={{asset("assets/Dashboard/vendor/datatables/css/select.bootstrap4.css")}}>
+    <link rel="stylesheet" type="text/css" href={{asset("assets/Dashboard/vendor/datatables/css/fixedHeader.bootstrap4.css")}}>
+    <link rel="stylesheet" type="text/css" href={{asset("assets/Dashboard/vendor/dropzone/dist/min/dropzone.min.css")}}>
+
+    <script src={{asset("assets/Dashboard/vendor/jquery/jquery-3.3.1.min.js")}}></script>
